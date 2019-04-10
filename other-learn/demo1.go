ff550ead87eea2d5b1a6ca68198c6db5ffc9399b
allen <824426699@qq.com>
2019-04-10 15:44:39 +0800

    chore: strings包学习

--- other-learn/demo1.go
+++ other-learn/demo1.go
@@ -2,7 +2,32 @@ package main
 
 import "fmt"
 
+// 这两个变量只能在该.go文件下访问
+var a, b = 10, 20
+
+// 这两个其他.go文件都可以访问
+var A, B = 10, 20
+
+// 全局变量范围>局部变量范围
+// 代码块里面的变量只能在代码块里面访问使用，
 func main() {
-	a := []int{0, 1, 2, 3, 4, 5, 6}
-	fmt.Println(a[:5])
+	var Num1 = 10
+	{
+		var num2 = 20
+		// 代码块里面可以访问到Num1
+		fmt.Println(Num1, num2)
+	}
+	// 代码块外面可以访问到Num1，但是访问不到num2
+	fmt.Println(Num1)
+
+	fmt.Println(addNum(a, b))
+	fmt.Println(multiply(A, B))
+}
+
+func addNum(a, b int) int {
+	return a + b
+}
+
+func multiply(a, b int) int {
+	return a * b
 }
