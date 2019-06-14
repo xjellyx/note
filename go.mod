0c5372fcf5fc8c5a9adc4024b4e716a9b2d14ea1
LnFen <824426699@qq.com>
2019-06-14 13:35:42 +0800

    ss

--- go.mod
+++ go.mod
@@ -2,6 +2,7 @@ module github.com/LnFen/note
 
 require (
 	git.yichui.net/tudy/go-bytebank v0.0.0-20190419094941-fb4cb8d3e112 // indirect
+	git.yichui.net/tudy/go-rest v1.3.17 // indirect
 	github.com/360EntSecGroup-Skylar/excelize v1.4.1 // indirect
 	github.com/LnFen/go-restful v0.0.0-20190401075119-9a4d0b30e342 // indirect
 	github.com/LnFen/graphql-api v0.0.0-20190424023100-739f0302897e // indirect
@@ -14,7 +15,9 @@ require (
 	github.com/gorilla/securecookie v1.1.1 // indirect
 	github.com/json-iterator/go v1.1.6 // indirect
 	github.com/kataras/iris v11.1.1+incompatible
-	github.com/suboat/sorm v0.2.1-0.20190412094245-cf3aeddc0c3c
+	github.com/suboat/sorm v0.2.1-0.20190529022223-aa087682bfb0
+	github.com/tealeg/xlsx v1.0.3 // indirect
+	github.com/tealeg/xlsx2csv v0.0.0-20170717074554-61833cb971d4 // indirect
 	github.com/tensorflow/tensorflow v1.13.1 // indirect
 	github.com/valyala/tcplisten v0.0.0-20161114210144-ceec8f93295a // indirect
 	golang.org/x/crypto v0.0.0-20190411191339-88737f569e3a // indirect
--- other-learn/go-tool.go
+++ other-learn/go-tool.go
@@ -1,69 +1,31 @@
 package main
 
 import (
-	"flag"
 	"fmt"
-	"github.com/golang/glog"
-	"golang.org/x/net/websocket"
-
-	"net/http"
+	"regexp"
+	"strings"
 )
 
-// 初始化函数
-func init() {
-	fmt.Println("Entry init")
-	// 连接池的  初始化； 数据结构的初始化；
-
-	flag.Set("alsologtostderr", "true") // 日志写入文件的同时，输出到stderr
-	flag.Set("log_dir", "./tmp")        // 日志文件保存目录
-	flag.Set("v", "3")                  // 配置V输出的等级。
-	flag.Parse()
-
-	return
+func Between(str, starting, ending string) string {
+	s := strings.Index(str, starting)
+	if s < 0 {
+		return ""
+	}
+	s += len(starting)
+	e := strings.Index(str[s:], ending)
+	if e < 0 {
+		return ""
+	}
+	return str[s : s+e]
 }
 
-type OnlineUser struct {
-	Connection *websocket.Conn
-}
-
-// 主函数
 func main() {
-	glog.Info("main")
-	fmt.Println("Entry main")
-	http.Handle("/Golangweb", websocket.Handler(BuildConnection))
-	if err := http.ListenAndServe(":7821", nil); err != nil {
-		fmt.Println("main err:", err.Error())
-		return
-	}
-	return
-}
+	b := "https://www.codedesigner.net/exec/1079/1902"
+	re, _ := regexp.Compile(`\d+`)
 
-func BuildConnection(ws *websocket.Conn) {
-	data := ws.Request().URL.Query().Get("data")
-	if data == "" {
-		fmt.Println("data is nil!!!")
-		//return
-	}
-	// 接受到的 客户端的数据
-	fmt.Println("data:", data)
-	// 处理-- 用户的登陆  用户的基础行为（走路 ， 开枪 ，死亡 等等）
-	onlineUser := &OnlineUser{
-		Connection: ws,
-	}
-	onlineUser.PullFromClient()
-}
-
-func (this *OnlineUser) PullFromClient() {
-
-	fmt.Println("PullFromClient")
-	for {
-		var content string
-		if err := websocket.Message.Receive(this.Connection, &content); err != nil {
-			break
-		}
-		if len(content) == 0 {
-			break
-		}
-		glog.Info("data:", content)
+	//查找符合正则的第一个
+	all := re.FindAll([]byte(b), -1)
+	for _, item := range all {
+		fmt.Println(string(item))
 	}
 }
